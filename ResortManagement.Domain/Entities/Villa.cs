88c2a1c812048fadd1dee0f331980da1a9d60d53
Farhan Mahmud <shahidinfo.45@gmail.com>
2025-04-29 01:02:25 +0600

    update villa feature

--- ResortManagement.Domain/Entities/Villa.cs
+++ ResortManagement.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,17 @@ namespace ResortManagement.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(60)]
+        [MinLength(3)]
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per night")]
+        [Range(10, 10000)]
         public double Price { get; set; }
+        [Range(1, 10)]
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
--- ResortManagement/Controllers/VillaController.cs
+++ ResortManagement/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ResortManagement.Domain.Entities;
 using ResortManagement.Infrastructure.Data;
 
 namespace ResortManagement.Web.Controllers
@@ -16,5 +17,50 @@ namespace ResortManagement.Web.Controllers
             var villas = _db.Villas.ToList();
             return View(villas);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa obj) 
+        {
+            if (obj.Name == obj.Description) 
+            {
+                ModelState.AddModelError("name","Description and Name can not be same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int villaId) 
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(x => x.Id == villaId);
+            //Villa? obj = _db.Villas.Find(villaId);
+            //var VillaList = _db.Villas.Where(x => x.Price < 50 && x.Occupancy > 0);
+            if (obj == null) 
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
     }
 }
